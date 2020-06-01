08ab9be79659e405d805d494a406b8e3a386e8a5
Broc Gailit <brocgailit@gmail.com>
2020-06-01 15:47:47 -0700

    Use raw output

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@
 shopt -s dotglob
 echo "Updating README"
 cat README.base.md > README.md
-printf "\n\n%s\n" "## View Compiled MJML" >> README.md
+printf "\n\n%s\n" "## Get Compiled MJML" >> README.md
 find src/* -prune -type d | while IFS= read -r d; do 
     echo "Building $d"
     mjml ./$d/index.mjml -o ./dist/${d:4}.html --config.minify
-    printf "%s\n" "- [${d:4}](./dist/${d:4}.html)" >> README.md
+    printf "%s\n" "- [${d:4}](./raw/${d:4}.html)" >> README.md
 done
 echo "Done!"
