e6844c71808da19b0b862e47c99e2c817e52a6d3
Broc Gailit <brocgailit@gmail.com>
2020-06-01 15:46:40 -0700

    Use .html extensions

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ printf "\n\n%s\n" "## View Compiled MJML" >> README.md
 find src/* -prune -type d | while IFS= read -r d; do 
     echo "Building $d"
     mjml ./$d/index.mjml -o ./dist/${d:4}.html --config.minify
-    printf "%s\n" "- [${d:4}](../blob/master/dist/${d:4})" >> README.md
+    printf "%s\n" "- [${d:4}](./dist/${d:4}.html)" >> README.md
 done
 echo "Done!"
